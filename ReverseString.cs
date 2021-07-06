449516cebc46acf3465d3b55c4f0192a4f7d1ee5
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2021-07-06 22:34:02 +0530

    Day 2 Asignments

---
+++ ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+
+            Console.WriteLine("Enter a string to reverse : ");
+
+            myString = Console.ReadLine();
+
+            Console.Write("Reverse String : ");
+            for (int i = myString.Length - 1; i >= 0; i--)
+            {
+                Console.Write(myString[i]);
+            }
+
+        }
+    }
+}
