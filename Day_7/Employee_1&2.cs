7fc46efd4010ef3221d276d8af3336e251ba0b04
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2021-07-13 22:49:44 +0530

    Day 7 assignments

---
+++ Day_7/Employee_1&2.cs
@@ -0,0 +1,94 @@
+﻿// Design an  Employee class with properties EmployeeId, Name, Designation. Have an encapsulated field called Salary. 
+// Expose method called GetSalary() which displays or returns the current salary,
+//     but setting of salary should be done at the time of hiring (object creation time).
+// Have an encapsulated method called IncreasePay() , where the salary can be increased by 10%.
+// The user / client  should be exposed only to a method called Promote() with a boolean value,
+// which decides the Increase Pay of 10% or not.
+
+using System;
+using System.Collections;
+
+namespace EmployeeProject
+{
+    class Employee
+    {
+        public int EmployeeId;
+        public string EmployeeName;
+        public string Designation;
+        private double Salary;
+        private int[] phone = new int[2];
+        public int this[int i]
+        {
+            get
+            {
+                return phone[i];
+            }
+            set
+            {
+                phone[i] = value;
+            }
+        }
+
+        public Employee(double salary)
+        {
+            this.Salary = salary;
+        }
+
+        public Employee(int id, string name, string designation, double salary)
+        {
+            this.EmployeeId = id;
+            this.EmployeeName = name;
+            this.Designation = designation;
+            this.Salary = salary;
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine($"{this.EmployeeName}\'s salary is {this.Salary}");
+        }
+
+        private void IncreasePay()
+        {
+            this.Salary += ((double)10 / 100) * this.Salary;
+            GetSalary();
+        }
+
+        public void Promote(bool isPromoted)
+        {
+            if (isPromoted)
+            {
+                IncreasePay();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = new Hashtable();
+
+            var temp = new Employee(1, "Sam", "Manager", 20000);
+
+            //indexer
+            temp[0] = 123456;
+            temp[1] = 654321;
+
+            employeeList.Add(temp.EmployeeId, temp);
+
+            Employee storedEmployee1 = (Employee)employeeList[temp.EmployeeId];
+
+
+            foreach (DictionaryEntry employee in employeeList)
+            {
+                Employee temp1 = (Employee)employee.Value;
+                Console.WriteLine($"Employee Name:{temp.EmployeeName}");
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"Contact Detail :{temp1[i]}");
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Day_7/Student_3.cs
@@ -0,0 +1,47 @@
+﻿// Write a program which has a class of Student with Id, Name and Date of Birth. 
+// You can set the Date of birth at the time of Admission (object creation),  and have a property which calculates the age.
+// (use property  get accessors ) and can be accessed from the client / main program. 
+// Also ensure user can not enter the Dob or Age at a later point of time in the program
+// Hint - Make use of Date time , time span we learnt on Day 5.  
+using System;
+
+namespace StudentProject
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public DateTime DOB { get; }
+
+        public int Age { get; }
+
+        public Student(int id, string name, string dob)
+        {
+            string[] date;
+            date = dob.Split(":");
+
+            this.StudentId = id;
+            this.StudentName = name;
+            this.DOB = new DateTime(int.Parse(date[0]), int.Parse(date[1]), int.Parse(date[2]), 0, 0, 0); ;
+
+            var today = DateTime.Now;
+            this.Age = today.Year - this.DOB.Year;
+
+            Console.WriteLine($"Admission for {name} was successfull");
+        }
+        public void StudentDetail()
+        {
+            Console.WriteLine($"Student Name:{this.StudentName}, Student Age:{this.Age}");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student(1, "Samuel", "1999:10:31");
+            student1.StudentDetail();
+
+        }
+    }
+}
