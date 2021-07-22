212cf35243acfc156b0f4fa164d71bbf16b2ddbb
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2021-07-22 13:09:37 +0530

    Events

---
+++ EVENTSPROJECT/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EVENTSPROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Video { Name = "MyVideo1" };
+            var videoEncoderObj = new VideoEncoder();
+
+            videoEncoderObj.myEvent += EmailService.SendEmail;
+            videoEncoderObj.Process(obj);
+            Console.ReadLine();
+        }
+    }
+    class Video
+    {
+        public string Name { get; set; }
+    }
+    class VideoEncoder
+    {
+        public delegate void PostProcessEventHandler(object source, EventArgs args);
+        public event PostProcessEventHandler myEvent;
+        public virtual void OnPostProcessingEvent()
+        {
+            if (myEvent != null)
+                myEvent(this, EventArgs.Empty);
+        }
+        public void Process(Video video)
+        {
+            Console.WriteLine($"Your video is processing {video.Name}");
+            OnPostProcessingEvent();
+        }
+    }
+    class EmailService
+    {
+        static public void SendEmail(object source, EventArgs args)
+        {
+            Console.WriteLine($"Sending email.....");
+        }
+    }
+}
