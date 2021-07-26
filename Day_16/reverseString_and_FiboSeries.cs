473f9378d829ee73ca70a2cfc861645bb3ee2d06
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2021-07-26 20:21:28 +0530

    Day_16

---
+++ Day_16/reverseString_and_FiboSeries.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+
+        static int ComputeFiboSeries(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            if (number == 0)
+            {
+                return 0;
+            }
+
+            return ComputeFiboSeries(number - 1) + ComputeFiboSeries(number - 2);
+        }
+
+        static string ReverseString(string text, int mid, int size)
+        {
+            var reverseString = text;
+            if (text.Length == 1)
+            {
+                return reverseString;
+            }
+
+            char[] s1 = new char[mid + 1];
+            char[] s2 = new char[size - mid];
+
+            text.CopyTo(0, s1, 0, mid + 1);
+            text.CopyTo(mid + 1, s2, 0, size - mid);
+
+            return ReverseString(new string(s2), (s2.Length - 1) / 2, s2.Length - 1) + ReverseString(new string(s1), (s1.Length - 1) / 2, s1.Length - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var result1 = ComputeFiboSeries(4);
+            Console.WriteLine($"{result1}");
+
+            string s1 = "potatooo";
+            var mid = (s1.Length - 1) / 2;
+            var size = s1.Length - 1;
+
+
+            var result2 = ReverseString(s1, mid, size);
+
+            Console.WriteLine($"{result2}");
+
+        }
+    }
+}
