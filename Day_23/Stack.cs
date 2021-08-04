ebdaee9f01c019cd35aa24b79247cdc9e1bab11a
thahseen21 <52756349+thahseen21@users.noreply.github.com>
2021-08-04 22:08:57 +0530

    Day_23

---
+++ Day_23/Stack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stack
+{
+    class Stack
+    {
+        public static void operator +(int[] stack, int top)
+        {
+            stack[top] = 10;
+        }
+    }
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int top = 0;
+            int[] stack = new int[10];
+
+            +Stack(stack, 0);
+
+        }
+    }
+}
---
+++ Day_23/Sum of elements.cs
@@ -0,0 +1,33 @@
+﻿//Time complexity is O(n)
+using System;
+using System.Collections.Generic;
+
+namespace SumOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> sumList = new List<int>();
+
+            int total = 0;
+
+            foreach (var num in list)
+            {
+                total += num;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sumList.Add(total - list[i]);
+            }
+
+            foreach (var num in sumList)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+        }
+    }
+}
