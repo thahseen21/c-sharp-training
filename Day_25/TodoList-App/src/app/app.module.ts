7e2e8a530f011f881e0e3c3ddaedc5bfb7035462
thahseen21 <thahseen.niaz@gmail.com>
2021-08-07 13:10:10 +0530

    todo-bare-styling

--- Day_25/TodoList-App/src/app/app.module.ts
+++ Day_25/TodoList-App/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoHeader } from './todo/header/header.component';
+import { TodoInput } from './todo/input/input.component';
+import { TodoTask } from './todo/task/task.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoComponent, TodoHeader, TodoInput, TodoTask],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Day_25/TodoList-App/src/app/todo/input/input.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  templateUrl: './input.component.html',
+})
+export class TodoInput {}
