aa3220d4692b28ba6b9c5c0740a1502afdb6279a
thahseen21 <thahseen.niaz@gmail.com>
2021-08-07 13:27:13 +0530

    basic layout completed

--- Day_25/TodoList-App/src/app/todo/input/input.component.ts
+++ Day_25/TodoList-App/src/app/todo/input/input.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'todo-input',
   templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
 })
 export class TodoInput {}
