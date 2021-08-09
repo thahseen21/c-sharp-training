060b880b1e0b05df6550d200fead10ee6fef9525
Mohammed Thahseen Niaz Ahamed <mohammedthahseenn@gislen.com>
2021-08-09 21:13:20 +0530

    queue

--- Day_26/Queue/Program.cs
+++ Day_26/Queue/Program.cs
@@ -5,16 +5,60 @@ namespace Queue
 
     public class Queue
     {
-        public class node
+        public class Node
         {
-            public int data;
+            public int Data;
+            public Node NextNode;
+            public Node()
+            {
+                NextNode = null;
+            }
+        }
+        Node Head;
+
+        public void Enqueue(int data)
+        {
+            var tempNode = new Node();
+            tempNode.Data = data;
+            tempNode.NextNode = Head;
+            Head = tempNode;
+        }
+        public void Dequeue()
+        {
+            var lastNode = new Node();
+            var beforeLastNode = new Node();
+            lastNode = Head;
+            while (lastNode.NextNode != null)
+            {
+                beforeLastNode = lastNode;
+                lastNode = lastNode.NextNode;
+            }
+            beforeLastNode.NextNode = null;
+        }
+        public void Display()
+        {
+            var tempNode = new Node();
+            tempNode = Head;
+            Console.Write("In ->");
+            while (tempNode != null)
+            {
+                Console.Write($"-{tempNode.Data}-");
+                tempNode = tempNode.NextNode;
+            }
+            Console.Write("->Out\n");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue list = new Queue();
+            list.Enqueue(10);
+            list.Enqueue(20);
+            list.Enqueue(30);
+            list.Display();
+            list.Dequeue();
+            list.Display();
         }
     }
 }
