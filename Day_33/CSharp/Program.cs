87d921cc991be74ea53011d041481b3280279071
Mohammed Tharzeez <mohammed@WSEINML-F8BXG3Q.local>
2021-08-19 17:21:33 +0530

    before

---
+++ Day_33/CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=42;
+            Type type = i.GetType();
+            Console.WriteLine(type);
+
+            Assembly info = typeof(int).Assembly;
+            Console.WriteLine($"{info}"); 
+        }
+    }
+}
